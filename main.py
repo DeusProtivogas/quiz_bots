399f18c4e3396ca3e73d991419a8f4fa6115d731
DeusProtivogas <kirill2689@gmail.com>
2024-07-01 20:54:01 +0300

    added get question button

--- main.py
+++ main.py
@@ -1,30 +1,54 @@
 import os
 import re
+import random
 
 import logging
 from dotenv import load_dotenv
 
-from telegram import Update, ForceReply
+from telegram import Update, ForceReply, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 
 logger = logging.getLogger(__name__)
 
+custom_keyboard = [['Новый вопрос', 'Сдаться'],
+                       ['Счет']]
 
 # Define a few command handlers. These usually take the two arguments update and
 # context.
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
     user = update.effective_user
+    context.user_data['q_n_a'] = read_file()
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
     update.message.reply_markdown_v2(
         fr'Hi {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
+        reply_markup=reply_markup,
     )
+
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
+
+
+def send_question(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    question, answer = random.choice(list(context.user_data['q_n_a'].items()))
+    print(question, answer)
+
+    # update.message.reply_markdown_v2(
+    #     fr'Hi {user.mention_markdown_v2()}\!',
+    #     reply_markup=ForceReply(selective=True),
+    # )
+
+    reply_markup = ReplyKeyboardMarkup(custom_keyboard)
+    update.message.reply_text(
+        text=f"{question}",
+        reply_markup=reply_markup)
+
+
 
 
 def echo(update: Update, context: CallbackContext) -> None:
@@ -39,9 +63,10 @@ def main() -> None:
     )
     load_dotenv()
     telegram_token = os.getenv('TELEGRAM_TOKEN')
+
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater(telegram_token)
+    updater = Updater(telegram_token, use_context=True)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
@@ -49,6 +74,9 @@ def main() -> None:
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
+
+    # dispatcher.add_handler(CommandHandler("new_question", send_question))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^Новый вопрос$'), send_question))
 
     # on non command i.e message - echo the message on Telegram
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
@@ -76,18 +104,18 @@ def read_file():
     for q_n_a in file_contents.split('\n\n')[3:]:
         # print('-', q_n_a, end='')
         if q_n_a.strip().startswith('Вопрос'):
-            q = re.split(r'Вопрос \d+:\n', q_n_a)[1]
+            q = re.split(r'Вопрос \d+:', q_n_a.replace('\n', ' '))[1]
             print('---', q)
             print(cntr)
             cntr += 1
         elif q_n_a.strip().startswith('Ответ'):
-            a = q_n_a.strip('Ответ:\n')
+            a = q_n_a.replace('\n', ' ').strip('Ответ:')
             print('--', a)
         if q and a:
             question_and_answer[q] = a
             a = None
             q = None
-    print(question_and_answer)
+    return question_and_answer
 
 
 if __name__ == '__main__':
