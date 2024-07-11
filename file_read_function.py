decc54bfcf45eb7438ff93fa0738a026bb606f45
DeusProtivogas <kirill2689@gmail.com>
2024-07-11 18:33:56 +0300

    renamed variables

--- file_read_function.py
+++ file_read_function.py
@@ -13,24 +13,19 @@ def read_file(folder = 'questions'):
         if filename.endswith('txt'):
             with open(os.path.join(questions_dir, filename), 'r', encoding='KOI8-R') as f:
                 file_contents = f.read()
-            cntr = 1
             # print(file_contents.split('\n\n'))
-            q = None
-            a = None
-            for q_n_a in file_contents.split('\n\n')[3:]:
+            question = None
+            answer = None
+            for question_answer in file_contents.split('\n\n')[3:]:
                 # print('-', q_n_a, end='')
-                if q_n_a.strip().startswith('Вопрос'):
-                    q = re.split(r'Вопрос \d+:', q_n_a.replace('\n', ' '))[1].strip()
-                    # print('---', q)
-                    # print(cntr)
-                    cntr += 1
-                elif q_n_a.strip().startswith('Ответ'):
-                    a = q_n_a.replace('\n', ' ').strip('Ответ:').strip()
-                    # print('--', a)
-                if q and a:
-                    question_and_answer[q] = a
-                    a = None
-                    q = None
+                if question_answer.strip().startswith('Вопрос'):
+                    question = re.split(r'Вопрос \d+:', question_answer.replace('\n', ' '))[1].strip()
+                elif question_answer.strip().startswith('Ответ'):
+                    answer = question_answer.replace('\n', ' ').strip('Ответ:').strip()
+                if question and answer:
+                    question_and_answer[question] = answer
+                    answer = None
+                    question = None
     return question_and_answer
 
 
