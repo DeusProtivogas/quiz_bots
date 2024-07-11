e4aa917e44b4fef2a15d86eb14b6637ef6000fc2
DeusProtivogas <kirill2689@gmail.com>
2024-07-11 18:57:06 +0300

    moved databases to bot level

--- telegram_bot.py
+++ telegram_bot.py
@@ -24,22 +24,15 @@ CUSTOM_KEYBOARD = [['Новый вопрос', 'Сдаться'],
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
 
-    redis_db = redis.Redis(
-        # host='redis-19024.c327.europe-west1-2.gce.redns.redis-cloud.com',
-        host=os.getenv('REDIS_HOST'),
-        port=os.getenv('REDIS_PORT'),
-        db=os.getenv('REDIS_DB'),
-        username=os.getenv('REDIS_USERNAME'),
-        password=os.getenv('REDIS_PASS'),
-    )
+
     # res = r.set('foo', 'bar')
     # # print(res)
     # print(r.get('foo'))
 
 
     user = update.effective_user
-    context.user_data['q_n_a'] = read_file(folder)
-    context.user_data['redis_db'] = redis_db
+    # context.user_data['q_n_a'] = read_file(folder)
+    # redis_db = redis_db
     reply_markup = ReplyKeyboardMarkup(CUSTOM_KEYBOARD)
     update.message.reply_markdown_v2(
         fr'Привет, {user.mention_markdown_v2()}\!',
@@ -56,18 +49,17 @@ def help_command(update: Update, context: CallbackContext) -> None:
 
 def send_question(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
-    question, answer = random.choice(list(context.user_data['q_n_a'].items()))
-    print(question, answer)
+    question, answer = random.choice(list(questions_and_answers.items()))
 
     # update.message.reply_markdown_v2(
     #     fr'Hi {user.mention_markdown_v2()}\!',
     #     reply_markup=ForceReply(selective=True),
     # )
+    print(update.effective_user.id, redis_db)
+    redis_db.set( update.effective_user.id, question )
 
-    context.user_data['redis_db'].set( update.effective_user.id, question )
-
-    print('1 ', context.user_data['redis_db'].get( update.effective_user.id ).decode("utf-8"))
-    print('2 ', context.user_data['q_n_a'].get( context.user_data['redis_db'].get( update.effective_user.id ).decode("utf-8") ))
+    print('1 ', redis_db.get( update.effective_user.id ).decode("utf-8"))
+    print('2 ', questions_and_answers.get( redis_db.get( update.effective_user.id ).decode("utf-8") ))
 
     reply_markup = ReplyKeyboardMarkup(CUSTOM_KEYBOARD)
     update.message.reply_text(
@@ -78,11 +70,9 @@ def send_question(update: Update, context: CallbackContext) -> None:
 
 
 def check_answer(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    print('Ans', update.message.text)
     reply_markup = ReplyKeyboardMarkup(CUSTOM_KEYBOARD)
-    if context.user_data['q_n_a'].get(
-            context.user_data['redis_db'].get(
+    if questions_and_answers.get(
+            redis_db.get(
                 update.effective_user.id
             ).decode("utf-8")
     ).split('.')[0] == update.message.text:
@@ -101,7 +91,7 @@ def surrender(update: Update, context: CallbackContext) -> None:
 
     reply_markup = ReplyKeyboardMarkup(CUSTOM_KEYBOARD)
     update.message.reply_text(
-        text=f"Правильный ответ - {context.user_data['q_n_a'].get( context.user_data['redis_db'].get( update.effective_user.id ).decode('utf-8') )}",
+        text=f"Правильный ответ - {questions_and_answers.get( redis_db.get( update.effective_user.id ).decode('utf-8') )}",
 
         reply_markup=reply_markup,
     )
@@ -112,7 +102,6 @@ NEW_QUESTION, SHOW_SCORE, GIVE_UP, CHECK_ANSWER, CHOICE = range(5)
 
 def main(folder) -> None:
 
-    load_dotenv()
     telegram_token = os.getenv('TELEGRAM_TOKEN')
 
     """Start the bot."""
@@ -144,28 +133,15 @@ def main(folder) -> None:
         fallbacks=[],
     )
     dispatcher.add_handler(conv_handler)
-
-    # on different commands - answer in Telegram
-    # dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
-    # dispatcher.add_handler(CommandHandler("new_question", send_question))
-    # dispatcher.add_handler(MessageHandler(Filters.regex('^Новый вопрос$'), send_question))
-
-    # on non command i.e message - echo the message on Telegram
-    # dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, check_answer))
-
-    # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
 if __name__ == '__main__':
-    # read_file()
+    load_dotenv()
     logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
         level=logging.INFO
@@ -175,5 +151,15 @@ if __name__ == '__main__':
     parser.add_argument('--folder', type=str, default='questions', help='Destination folder (default: questions)')
     args = parser.parse_args()
     folder = args.folder
+
+    questions_and_answers = read_file(folder)
+
+    redis_db = redis.Redis(
+        host=os.getenv('REDIS_HOST'),
+        port=os.getenv('REDIS_PORT'),
+        db=os.getenv('REDIS_DB'),
+        username=os.getenv('REDIS_USERNAME'),
+        password=os.getenv('REDIS_PASS'),
+    )
 
     main(folder)
