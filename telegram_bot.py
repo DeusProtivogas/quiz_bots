56e2efc38e1335f53826de46acee47a5436d3a6e
DeusProtivogas <kirill2689@gmail.com>
2024-08-04 20:59:47 +0500

    removed global variables

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,12 +3,9 @@ import re
 import random
 import redis
 import argparse
-
 import logging
 from dotenv import load_dotenv
-
 from file_read_function import read_file
-
 from telegram import Update, ForceReply, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
 
@@ -20,60 +17,81 @@ CUSTOM_KEYBOARD = [['Новый вопрос', 'Сдаться'],
                    ['Счет']]
 
 
+class QuizBot:
+    def __init__(self, folder, redis_config, telegram_token):
+        self.questions_and_answers = read_file(folder)
+        self.redis_db = redis.Redis(**redis_config)
+        self.telegram_token = telegram_token
+        self.updater = Updater(self.telegram_token, use_context=True)
+        self.dispatcher = self.updater.dispatcher
+        self.setup_handlers()
 
-def start(update: Update, context: CallbackContext) -> None:
-    user = update.effective_user
-    reply_markup = ReplyKeyboardMarkup(CUSTOM_KEYBOARD)
+    def setup_handlers(self):
+        conv_handler = ConversationHandler(
+            entry_points=[CommandHandler("start", self.start)],
 
+            states={
+                CHOICE: [
+                    MessageHandler(Filters.regex('Новый вопрос'), self.send_question),
+                    MessageHandler(Filters.regex('Сдаться$'), self.surrender),
+                ],
+                NEW_QUESTION: [MessageHandler(Filters.regex('Новый вопрос'), self.send_question)],
+                GIVE_UP: [MessageHandler(Filters.regex('Сдаться'), self.surrender)],
+                CHECK_ANSWER: [
+                    MessageHandler(Filters.regex('Новый вопрос'), self.send_question),
+                    MessageHandler(Filters.regex('Сдаться'), self.surrender),
+                    MessageHandler(Filters.text, self.check_answer)
+                ],
+            },
 
+            fallbacks=[],
+        )
+        self.dispatcher.add_handler(conv_handler)
 
-    update.message.reply_markdown_v2(
-        fr'Привет, {user.mention_markdown_v2()}\!',
-        reply_markup=reply_markup,
-    )
-    return CHOICE
-
-def send_question(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    question, answer = random.choice(list(questions_and_answers.items()))
-
-    redis_db.set( update.effective_user.id, question )
-
-    update.message.reply_text(
-        text=f"{question}",
-    )
-    return CHECK_ANSWER
-
-
-def check_answer(update: Update, context: CallbackContext) -> None:
-    reply_markup = ReplyKeyboardMarkup(CUSTOM_KEYBOARD)
-    if questions_and_answers.get(
-            redis_db.get(
-                update.effective_user.id
-            ).decode("utf-8")
-    ).split('.')[0] == update.message.text:
-        update.message.reply_text(
-            text=f"Поздравляю! Для следующего вопроса нажми «Новый вопрос»",
+    def start(self, update: Update, context: CallbackContext) -> None:
+        user = update.effective_user
+        reply_markup = ReplyKeyboardMarkup(CUSTOM_KEYBOARD)
+        update.message.reply_markdown_v2(
+            fr'Привет, {user.mention_markdown_v2()}\!',
             reply_markup=reply_markup,
         )
-    else:
+        return CHOICE
+
+    def send_question(self, update: Update, context: CallbackContext) -> None:
+        question, answer = random.choice(list(self.questions_and_answers.items()))
+        self.redis_db.set(update.effective_user.id, question)
+        update.message.reply_text(text=f"{question}")
+        return CHECK_ANSWER
+
+    def check_answer(self, update: Update, context: CallbackContext) -> None:
+        reply_markup = ReplyKeyboardMarkup(CUSTOM_KEYBOARD)
+        question = self.redis_db.get(update.effective_user.id).decode("utf-8")
+        correct_answer = self.questions_and_answers.get(question).split('.')[0]
+        if correct_answer == update.message.text:
+            update.message.reply_text(
+                text=f"Поздравляю! Для следующего вопроса нажми «Новый вопрос»",
+                reply_markup=reply_markup,
+            )
+        else:
+            update.message.reply_text(
+                text=f"Неправильно… Попробуешь ещё раз?",
+                reply_markup=reply_markup,
+            )
+        return CHOICE
+
+    def surrender(self, update: Update, context: CallbackContext) -> None:
+        reply_markup = ReplyKeyboardMarkup(CUSTOM_KEYBOARD)
+        question = self.redis_db.get(update.effective_user.id).decode("utf-8")
+        correct_answer = self.questions_and_answers.get(question)
         update.message.reply_text(
-            text=f"Неправильно… Попробуешь ещё раз?",
+            text=f"Правильный ответ - {correct_answer}",
             reply_markup=reply_markup,
         )
-    return CHOICE
+        self.send_question(update, context)
 
-
-def surrender(update: Update, context: CallbackContext) -> None:
-
-    reply_markup = ReplyKeyboardMarkup(CUSTOM_KEYBOARD)
-    update.message.reply_text(
-        text=f"Правильный ответ - {questions_and_answers.get( redis_db.get( update.effective_user.id ).decode('utf-8') )}",
-
-        reply_markup=reply_markup,
-    )
-    send_question(update, context)
-
+    def run(self):
+        self.updater.start_polling()
+        self.updater.idle()
 
 
 if __name__ == '__main__':
@@ -88,43 +106,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
     folder = args.folder
 
-    questions_and_answers = read_file(folder)
-
-    redis_db = redis.Redis(
-        host=os.getenv('REDIS_HOST'),
-        port=os.getenv('REDIS_PORT'),
-        db=os.getenv('REDIS_DB'),
-        username=os.getenv('REDIS_USERNAME'),
-        password=os.getenv('REDIS_PASS'),
-    )
+    redis_config = {
+        'host': os.getenv('REDIS_HOST'),
+        'port': os.getenv('REDIS_PORT'),
+        'db': os.getenv('REDIS_DB'),
+        'username': os.getenv('REDIS_USERNAME'),
+        'password': os.getenv('REDIS_PASS')
+    }
 
     telegram_token = os.getenv('TELEGRAM_TOKEN')
 
-    updater = Updater(telegram_token, use_context=True)
-
-    dispatcher = updater.dispatcher
-
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
-
-        states={
-            CHOICE: [
-                MessageHandler(Filters.regex('Новый вопрос'), send_question),
-                MessageHandler(Filters.regex('Сдаться$'), surrender),
-            ],
-            NEW_QUESTION: [MessageHandler(Filters.regex('Новый вопрос'), send_question)],
-            GIVE_UP: [MessageHandler(Filters.regex('Сдаться'), surrender)],
-            CHECK_ANSWER: [
-                MessageHandler(Filters.regex('Новый вопрос'), send_question),
-                MessageHandler(Filters.regex('Сдаться'), surrender),
-                MessageHandler(Filters.text, check_answer)
-            ],
-        },
-
-        fallbacks=[],
-    )
-    dispatcher.add_handler(conv_handler)
-
-    updater.start_polling()
-
-    updater.idle()
+    bot = QuizBot(folder, redis_config, telegram_token)
+    bot.run()
